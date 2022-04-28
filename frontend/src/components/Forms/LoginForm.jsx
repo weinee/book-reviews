3a917fb115abfc6b8583e6eba4910004a44affd6
weinee <weinee24@gmail.com>
2022-04-28 16:59:18 +0800

    Connect to sign in endpoints

--- frontend/src/components/Forms/LoginForm.jsx
+++ frontend/src/components/Forms/LoginForm.jsx
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { signIn } from "../../actions/auth.js";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 const LoginForm = () => {
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogIn = (e) => {
+    e.preventDefault();
+    dispatch(signIn(formData, navigate));
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form onSubmit={handleLogIn}>
+      <Form.Group className="mb-3">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          id="email"
+          onChange={handleChange}
+        />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          id="password"
+          onChange={handleChange}
+        />
         <Form.Text className="d-flex text-muted">
           Forgot your password?
         </Form.Text>
